b76d96fcef2131b167b019236d6c0f03e3f81c48
Joshua Johnson <jrjohnson91@Joshua-Johnsons-MacBook-Pro.local>
2025-02-22 15:07:34 -0500

    added some content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,13 @@
 
 export default function About() {
     return (
-        <h1>About</h1>
+        <main>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, modi quos molestiae, quam sequi culpa enim reprehenderit animi officia sit deleniti delectus, maxime praesentium temporibus fuga ullam. Laborum, earum repudiandae!</p>
+            <p>Dolore vel quis repudiandae nihil provident? Iure laboriosam aperiam facilis totam deserunt ipsum inventore quis quia. Nulla eum blanditiis quis, voluptatum rem, sint temporibus dolore assumenda deleniti accusantium quas et.</p>
+            <p>Facere sequi deleniti, nostrum minus quas sunt omnis est similique ducimus eligendi explicabo itaque, debitis numquam reiciendis perferendis consequatur obcaecati sapiente laboriosam quos dolorem laudantium nesciunt. Facere nemo consequuntur alias?</p>
+            <p>Ducimus aperiam, deleniti doloribus mollitia reprehenderit dicta iste iure error alias natus placeat aliquid numquam in ut ipsum esse nulla eveniet doloremque laboriosam fugiat voluptas! Saepe porro commodi rerum accusamus!</p>
+            <p>Aspernatur veritatis deserunt, fuga, voluptate ducimus, corrupti totam nostrum nihil explicabo commodi pariatur perspiciatis sequi vitae ipsam recusandae accusamus cum est ex consectetur. Ut doloremque nobis consequuntur, dolorum aut pariatur.</p>
+        </main>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
-
+import orthLogo from '../assets/images/orth-logo.svg'
+import './App.css'
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <img src={orthLogo} />
+            <h1>Home</h1>
+        </>
     )
 }
